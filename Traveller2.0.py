60704bf36ec5980585f5360a9200901b5a56fd6a
Nsf03 <nazarova.sf@phystech.edu>
2021-12-01 13:46:01 +0300

    Убрала баг со стенами

--- Traveller2.0.py
+++ Traveller2.0.py
@@ -30,6 +30,7 @@ bows = []
 arrows = []
 units = []
 walls = []
+enemies = []
 
 
 class Sword:
@@ -133,6 +134,8 @@ class Wall:
                 arrows[j].y = self.y
                 arrows[j].speed = 0
         for k in range(len(units)):
+            pass
+        for k in range(len(units)):
             if units[k].x + units[k].width > self.x and units[k].x < self.x + self.w and units[
                 k].y < self.y + self.h and units[k].y + units[k].height > self.y and units[k].orientation == 'right':
                 units[k].x = self.x - units[k].width
@@ -163,10 +166,8 @@ class Unit:
         self.bow = bow
         self.buttons = buttons
 
-    def stay(self, walls):
-        '''
-        отрисовываем танк
-        '''
+    def stay(self):
+
         rect(screen, 'grey', (self.x, self.y, self.width, self.height))
         rect(screen, 'green',
              (self.x + 0.25 * self.width, self.y + self.height + 5, self.width * 0.5 * self.hp / 100 + 1, 10))
@@ -176,17 +177,6 @@ class Unit:
             self.hold_a_sword()
         if self.weapon == 'bow':
             self.hold_a_bow()
-        for i in range(len(walls)):
-            if not (self.x + self.width > walls[i].x and self.x < walls[i].x + walls[i].w and self.y < walls[i].y +
-                    walls[i].h and self.y + self.height > walls[i].y):
-                if self.Vx > 0:
-                    self.orientation = 'right'
-                if self.Vx < 0:
-                    self.orientation = 'left'
-                if self.Vy > 0:
-                    self.orientation = 'bot'
-                if self.Vy < 0:
-                    self.orientation = 'top'
 
     def acquire(self, swords, bows):
         for i in range(len(swords)):
@@ -334,15 +324,83 @@ class Unit:
         if self.hp <= 0:
             units.remove(units[i])
 
+class Enemy(Unit):
+    ''' Класс врагов'''
+    def stay(self):
+        '''
+        отрисовываем врага
+        '''
+        rect(screen,'red',(self.x,self.y,self.width,self.height))
+        rect(screen,'green',(self.x+0.25*self.width,self.y+self.height+5,self.width*0.5*self.hp/100+1,10))
+        rect(screen,'red',(self.x+0.25*self.width+self.width*0.5*self.hp/100,self.y+self.height+5,self.width*0.5*(100-self.hp)/100+1,10))
+        if self.weapon == 'sword':
+            self.hold_a_sword()
+        if self.weapon == 'bow':
+            self.hold_a_bow()
+    def move(self):
+        ''' Движение врага по горизонтали (потом добавлю другие возможности)'''
+        if self.orientation == 'right':
+            self.Vx = 3
+            self.x += self.Vx
+            if self.x >= 300:
+                self.orientation = 'left'
+        if self.orientation == 'left':
+            self.Vx = 3
+            self.x -= self.Vx
+            if self.x <= 0:
+                self.orientation = 'right'
+    def hold_a_sword(self):
+        '''Меч врага всегда в режиме убийства'''
+        if self.orientation == 'right':
+            self.sword.x0 = self.x+self.width
+            self.sword.y0 = self.y+self.height/2
+            self.sword.x1 = self.x+self.width+self.sword.l
+            self.sword.y1 = self.y+self.height/2
+        if self.orientation == 'left':
+            self.sword.x0 = self.x
+            self.sword.y0 = self.y+self.height/2
+            self.sword.x1 = self.x-self.sword.l
+            self.sword.y1 = self.y+self.height/2
+        if self.orientation == 'top':
+            self.sword.x0 = self.x+self.width/2
+            self.sword.y0 = self.y
+            self.sword.x1 = self.width/2+self.x
+            self.sword.y1 = self.y-self.sword.l
+        if self.orientation == 'bot':
+            self.sword.x0= self.x+self.width/2
+            self.sword.y0 = self.y+self.height
+            self.sword.x1 = self.width/2+self.x
+            self.sword.y1 = self.y+self.height+self.sword.l
+        line(screen,(0,0,0),(self.sword.x0,self.sword.y0),
+                (self.sword.x1, self.sword.y1),3)
+        self.sword.sharp = 1
+    def damage(self,arrows,swords,enemies):
+        '''Получение урона врагом (урон выше, чем у игрока)'''
+        for j in range(len(arrows)-1,-1,-1):
+            if arrows[j].x > self.x and arrows[j].x < self.x+self.width and arrows[j].y>self.y and arrows[j].y<self.y+self.height:
+                self.hp -= 10+abs(arrows[j].speed)
+                arrows.remove(arrows[j])
+        for k in range(len(swords)):
+            if ((swords[k].x1 > self.x and swords[k].x1 < self.x+self.width and swords[k].y1>self.y and swords[k].y1<self.y+self.height) or
+                ((swords[k].x1+swords[k].x0)/2 > self.x and (swords[k].x1+swords[k].x0)/2 < self.x+self.width and (swords[k].y1+swords[k].y0)/2>self.y and (swords[k].y1+swords[k].y0)/2>self.y<self.y+self.height)) and self != swords[k].owner and swords[k].sharp == 1:
+                self.hp -= 30
+                swords[k].phi -= 0.1
+                swords[k].sharp = 0
+        if self.hp<=0:
+            enemies.remove(enemies[i])
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+enemies.append(Enemy(150, 150, 60, 50, 0, 0, 3, 'right', 60, 'sword', None, None, 0))
 units.append(Unit(10, 300, 50, 40, 0, 0, 5, 'right', 75, 'sword', None, None, ('w', 's', 'a', 'd')))
 units.append(Unit(300, 300, 50, 40, 0, 0, 10, 'right', 75, None, None, None, None))
 swords.append(Sword(0, 0, 0, 0, 50, 5 * m.pi / 12, 0, units[0]))
 bows.append(Bow(50, 25, 0, 0, units[0]))
+swords.append(Sword(0,0,0,0,0.05,5*m.pi/12,0,enemies[0]))
 walls.append(Wall(500, 600, 300, 300))
+for i in range(len(enemies)):
+    enemies[i].acquire(swords,bows)
 for i in range(len(units)):
     units[i].acquire(swords, bows)
 while not finished:
@@ -350,8 +408,12 @@ while not finished:
     for i in range(len(walls)):
         walls[i].stay()
         walls[i].collision(arrows, units)
+    for i in range(len(enemies)-1,-1,-1):
+        enemies[i].stay()
+        enemies[i].move()
+        enemies[i].damage(arrows,swords,enemies)
     for i in range(len(units) - 1, -1, -1):
-        units[i].stay(walls)
+        units[i].stay()
         units[i].move()
         units[i].damage(arrows, swords, units)
     for i in range(len(arrows)):
